e8b4d21dbd75e7b4e6f18bdedd370eb5564b437e
Garlinsk <franklinwanami@gmail.com>
2021-06-28 22:15:04 +0300

    added style

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
-$("form").submit(function(event) {
-    event.preventDefault();
-    let name = $("#name").val();
-    let email = $("#email").val();
-    let message = $("#message").val();
-    let phoneNumber = $("#number").val();
-    let location = $("#message").val();
+$(document).ready(function() {
+    $("form").submit(function(event) {
+        event.preventDefault();
+        let name = $("#name").val();
+        let email = $("#email").val();
+        let location = $("#location").val();
+        let phoneNumber = $("#number").val();
+        
 
-    if (name == "") {
-        alert("please enter your name correctly");
-    }else if (email == "") {
-        alert("please enter your email correctly");
-    }else if (message == "") {
-        alert("please enter a message");
-    } else if (phoneNumber == "") {
-        alert("please enter a phonenumber");   
-    }else {
-        alert("Hi " + name + "we have received your message.Thank you for shopping with us.")
-    }
-    
+        if (name == "") {
+            alert("please enter your name correctly");
+        }else if (email == "") {
+            alert("please enter your email correctly");
+        }else if (message == "") {
+            alert("please enter a message");
+        }else {
+            alert("Hi " + name + "Thank you for shopping with us.A confirmation message will be sent to confirm your order")
+        }
+    });
 });
