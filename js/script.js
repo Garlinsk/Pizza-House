de567a93dda5705cef12b3df5f597f8db02f9b6c
Garlinsk <franklinwanami@gmail.com>
2021-06-28 22:51:53 +0300

    modified js/script.js and index.html

--- js/script.js
+++ js/script.js
@@ -6,15 +6,19 @@ $(document).ready(function() {
         let location = $("#location").val();
         let phoneNumber = $("#number").val();
         
-
+        
         if (name == "") {
             alert("please enter your name correctly");
         }else if (email == "") {
             alert("please enter your email correctly");
         }else if (message == "") {
             alert("please enter a message");
+        }else if (location == "") {
+            alert("please enter location");
+        }else if (phoneNumber == "") {
+                alert("please enter Your number");   
         }else {
-            alert("Hi " + name + "Thank you for shopping with us.A confirmation message will be sent to confirm your order")
+            alert("Hi " + name +   "Thank you for shopping with us.A confirmation message will be sent to confirm your order")
         }
     });
 });
